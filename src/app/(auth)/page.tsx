6a512c158100818a9cb060f6d14c2b8e04effde0
InhwanCho <whdlsghks2@gmail.com>
2024-11-06 11:33:51 +0900

    change auth img

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -7,16 +7,9 @@ import CenterLogo from "../_components/center-logo";
 export default function AuthPage() {
   return (
     <div className="w-[1920px] h-[1080px] relative bg-[#19161d]">
-      <img alt='pic' className="w-[206px] h-52 left-[270px] bottom-0 absolute" src="/assets/auth/Asset91.png" />
-      <img alt='pic' className="w-[408.63px] h-[403.72px] left-[-48.63px] bottom-0 absolute " src="/assets/auth/Asset15.png" />
-      <img alt='pic' className="w-[229.95px] h-[226px] left-[1652px] top-[38px] absolute" src="/assets/auth/Asset102.png" />
-      <img alt='pic' className="w-[94.98px] h-[341.65px] right-0 top-[481.21px] absolute" src="/assets/auth/Asset86.png" />
-      <img alt='pic' className="w-[284.45px] h-[288.18px] right-0 bottom-0 absolute" src="/assets/auth/Asset42.png" />
-      <img alt='pic' className="w-[94.76px] h-[84.49px] left-[1755.98px] top-[755.90px] absolute " src="/assets/auth/Asset148.png" />
-      <img alt='pic' className="w-[159.11px] h-[144.15px] left-[183.98px] top-[86.82px] absolute" src="/assets/auth/Asset149.png" />
-      <img alt='pic' className="w-[97.17px] h-[448.62px] left-0 top-[151px] absolute" src="/assets/auth/Asset82.png" />
+      <img src="/assets/auth/이메일로그인.webp" alt="hero image" />
       <div className="w-[640px] h-[809px] left-[1040px] top-[183px] absolute bg-white rounded-2xl backdrop-blur-[34px]" />
-      <CenterLogo/>
+      
       <div className="w-[460px] h-[340px] left-[1130px] top-[418px] absolute">
         <div className="w-[460px] h-[70px] left-0 top-[94px] absolute flex-col justify-start items-start gap-[15px] inline-flex">
           <div className="self-stretch h-[70px] px-5 py-6 bg-white rounded-2xl border border-[#b4b4b4] flex-col justify-start items-start flex">
