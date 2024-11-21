fc44b88108fab0e3cee670e667596e1c8111a79a
InhwanCho <whdlsghks2@gmail.com>
2024-11-21 11:37:36 +0900

    auth , detail page in progress

--- src/app/challenge/[slug]/page.tsx
+++ src/app/challenge/[slug]/page.tsx
@@ -9,7 +9,7 @@ export default async function ChallengeDetailPage({ params }: ChallengeDetailPag
   const { slug } = await params;
 
   return (
-    <main className="max-w-7xl mt-20 w-full mx-auto px-5 xl:px-0 md:px-20">
+    <main className="max-w-7xl mt-16 lg:mt-20 w-full mx-auto px-4 xl:px-4 md:px-[74px] lg:px-[32px]">
       <ChallengeHeader slug={slug} />
       <ChallengeContent slug={slug} />
     </main>
