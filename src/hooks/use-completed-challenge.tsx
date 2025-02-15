90d2b1e91a5b5e09cc0905077d226ba303bed142
InhwanCho <whdlsghks2@gmail.com>
2025-02-15 11:31:48 +0900

    regen access test

--- src/hooks/use-completed-challenge.tsx
+++ src/hooks/use-completed-challenge.tsx
@@ -6,6 +6,7 @@ interface CompletedChallengeData {
   challengeShortIntro: string;
   challengeTitle: string;
   memberCount: string;
+  challengeDay: string;
 }
 
 export function useCompletedChallenge(
